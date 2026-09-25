5134faab669f474c6a866ef62789c47dea6c87cc
Kushal Shakya <kushalshakya@MacBook-Air-Kushal.local>
2026-09-25 15:12:08 +0545

    fix: convert Python folder from submodule to regular folder

---
+++ Python/random.py
@@ -0,0 +1,10 @@
+import random
+boy_name = input("Enter a boy's name: ")
+girl_name = input("Enter a girl's name: ")
+love_score = random.randint(1, 100)
+if love_score > 70:
+    print("Your love score is " + str(love_score) + "%, you are made for each other!")
+elif love_score > 30 and love_score <= 70:
+    print("Your love score is " + str(love_score) + "%, you are good together.")
+else:
+    print("Your love score is " + str(love_score) + "%, you might want to work on your relationship.")
