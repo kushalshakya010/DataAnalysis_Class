55de8040204e00b628290e3eab5196e86564d5c6
Kushal Shakya <kushalshakya@MacBook-Air-Kushal.local>
2026-09-26 22:12:59 +0545

    random_number was renamed to coorect issues

--- Python/random_number.py
+++ Python/random_number.py
@@ -1,10 +1,13 @@
 import random
+
 boy_name = input("Enter a boy's name: ")
 girl_name = input("Enter a girl's name: ")
+
 love_score = random.randint(1, 100)
+
 if love_score > 70:
     print("Your love score is " + str(love_score) + "%, you are made for each other!")
 elif love_score > 30 and love_score <= 70:
-    print("Your love score is " + str(love_score) + "%, you are good together.")
+    print("Your love score is " + str(love_score) + "%, you are still good together.")
 else:
     print("Your love score is " + str(love_score) + "%, you might want to work on your relationship.")
